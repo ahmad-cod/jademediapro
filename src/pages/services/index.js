a6d352e1f5d5fab48077d02679d2b7d5f451a82c
Silver Idahosa <108458852+SilverBeing@users.noreply.github.com>
2023-01-14 15:46:10 +0100

    Update

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { serviceData } from "../../data/serviceData";
 
-import ServiceHeader from "../../components/sevicesComponet/serviceHeader";
+import ServiceHeader from "../../components/servicesComponent/serviceHeader";
 import ReadyToWork from "../../components/ReadyToWork";
 import ServicesComponent from "../../components/Services";
-import Navigation from "../../components/sevicesComponet/serviceNavigation";
-import Work from "../../components/sevicesComponet/work";
+import Navigation from "../../components/servicesComponent/serviceNavigation";
+import Work from "../../components/servicesComponent/work";
 import styles from "../../styles/Project.module.css";
 
 const Services = () => {
