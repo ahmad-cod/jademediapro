1a7f2f0337d71fb64e81dc5314f984cfee2b166b
Nwachukwu Anthony <105246737+CoolTonie@users.noreply.github.com>
2023-02-04 22:14:58 +0100

    job page refix

--- src/pages/job.js
+++ src/pages/job.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { jobheroimg } from "../images";
 import Image from "next/image";
 
+import { jobdata } from "../data/jobData";
+
 export default function Terms() {
   return (
     <div>
@@ -19,167 +21,177 @@ export default function Terms() {
         </div>
       </div>
       <div className=" flex gap-[163px] px-[20px] py-[31px] md:pt-12 md:pr-[104px] md:pb-[132px] md:pl-[105px] w-full">
+        {/* <div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+            <div>
+              <h2 className="font-normal text-4xl ">
+                GRAPHIC <br /> DESIGNER
+              </h2>
+            </div>
+            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+              <img src="/border-line.png" />
+              <div className="flex gap-52 items-center font-normal text-[15px]">
+                <div>
+                  <p>New Jersey</p>
+                  <p>Contract</p>
+                  <p>Crane Inc.</p>
+                </div>
+                <div>
+                  <img src="/white-arrow.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
         <div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
-            <div>
-              <h2 className="font-normal text-4xl ">
-                GRAPHIC <br /> DESIGNER
-              </h2>
-            </div>
-            <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
-              <img src="/border-line.png" />
-              <div className="flex gap-52 items-center font-normal text-[15px]">
-                <div>
-                  <p>New Jersey</p>
-                  <p>Contract</p>
-                  <p>Crane Inc.</p>
-                </div>
-                <div>
-                  <img src="/white-arrow.png" />
-                </div>
-              </div>
-            </div>
-          </div>
+          {jobdata.map((item, i) => {
+            <JobCard
+              title={item.title}
+              location={item.location}
+              work_type={item.work_type}
+              industry={item.industry}
+            />;
+          })}
         </div>
         <div>
           <div>
@@ -225,6 +237,34 @@ export default function Terms() {
               <button>Sign up</button>
               <img src="/lemon-arrow.png" className="w-4 h-[14px]" />
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function JobCard(props) {
+  let titlesplit = props.title.split(" ");
+
+  return (
+    <div className="flex bg-[#3A9F94] rounded px-6 pt-2 pb-4 items-center gap-28 text-[#f6f6f6] cursor-pointer mb-10 hover:bg-[#156e64] ">
+      <div>
+        <h2 className="font-normal text-4xl ">
+          GRAPHIC <br /> DESIGNER
+          {titlesplit[0]} <br /> {titlesplit[1]}
+        </h2>
+      </div>
+      <div className="flex gap-28 hover:translate-x-2 hover:duration-0 hover:transition hover:md:duration-700">
+        <img src="/border-line.png" />
+        <div className="flex gap-52 items-center font-normal text-[15px]">
+          <div>
+            <p>{props.location}</p>
+            <p>{props.work_type}</p>
+            <p>{props.industry}</p>
+          </div>
+          <div>
+            <img src="/white-arrow.png" />
           </div>
         </div>
       </div>
